24f705d870011c5c4c33063af9c4a7c5a7e7e7e3
Afsal <afsalpx@gmail.com>
2022-03-13 16:48:17 +0530

    logo changes

--- resources/themes/velocity/views/layouts/header/index.blade.php
+++ resources/themes/velocity/views/layouts/header/index.blade.php
@@ -1,7 +1,7 @@
 <header class="sticky-header">
     <div class="row remove-padding-margin velocity-divide-page">
         <a class="left navbar-brand" href="{{ route('shop.home.index') }}" aria-label="Logo">
-            <img class="logo" src="{{ core()->getCurrentChannel()->logo_url ?? asset('themes/velocity/assets/images/nav-logo.png') }}" alt="" />
+            <img class="logo" src="{{ core()->getCurrentChannel()->logo_url ?? asset('themes/velocity/assets/images/nav-logo4.png') }}" alt="" />
         </a>
 <!-- dsadas -->
         <div class="right searchbar">
